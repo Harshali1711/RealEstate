469f621e58dd305acda5f828cb595ffb457387d5
Harshali <mehtaharshali17@gmail.com>
2021-02-25 15:12:57 +1100

    added runner file and testcase

--- src/test/java/com/RealEstate/testCases/AgentInfoTest.java
+++ src/test/java/com/RealEstate/testCases/AgentInfoTest.java
@@ -1,11 +1,8 @@
 package com.RealEstate.testCases;
 
-
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.RealEstate.pageObjects.AgentInfoPage;
@@ -13,48 +10,48 @@ import com.RealEstate.pageObjects.HomePage;
 import com.RealEstate.pageObjects.PropertiesForSalePage;
 import com.RealEstate.testBase.TestBase;
 
+public class AgentInfoTest extends TestBase{
 
-
-public class PropertiesForSaleTest extends TestBase {
-	
 	HomePage homepage;
 	PropertiesForSalePage propsale;
 	AgentInfoPage agentinfo;
-	
-	public PropertiesForSaleTest()
+	String Agentname;
+
+	public AgentInfoTest()
 	{
 		super();
 	}
-	
+
 	@BeforeClass
-	public void Setup()
+	public void Setup() throws Exception
 	{
 		initialization();
 		homepage=new HomePage();
 		homepage.search_Property("London");
-	}
-	
-	@Test
-	public void List_all_properties_Test() throws InterruptedException
-	{
 		propsale=new PropertiesForSalePage();
-		propsale.List_all_properties();		
-	}
-	
-	@Test
-	public void Verify_AgentDetailsTest() throws InterruptedException
-	{
-		String Agentname=propsale.Agent_ContactDetails();
+		propsale.List_all_properties();	
+		Agentname=propsale.Agent_ContactDetails();
 		System.out.println(Agentname);
 		propsale.AgentINFO();
+		Thread.sleep(2000);
+		agentinfo=new AgentInfoPage();
 	}
-	
+
+	@Test
+	public void verifyAgentLabel()
+	{
+		String AgentInfoName=agentinfo.AgentLabel();
+		System.out.println(AgentInfoName);
+
+		Assert.assertEquals(Agentname, AgentInfoName);
+		System.out.println("Agent has been verified");
+	}
+
 	@AfterClass
 	public void tearDown()
 	{
 		driver.close();
 	}
 
-	
 
 }
